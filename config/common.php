33a63f26a4f3dbd86b8283869d4159936b63d9af
kgssandeepa <sonalsandeepa08@gmail.com>
2025-12-19 10:09:49 +0530

    Create Common.php

---
+++ config/common.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    'imagesSizes' => [
+        ['name' => 'original', 'quality' => 100, 'convertable' => false],
+        ['name' => 'medium', 'quality' => 50, 'convertable' => true],
+        ['name' => 'thumbnail', 'quality' => 10, 'convertable' => true]
+    ],
+    // 'quotation_expiration_period'=>14,
+    // 'first_user_username'=>env('FIRST_USER_USERNAME'),
+    // 'first_user_password'=>env('FIRST_USER_PASSWORD'),
+    // 'generic_error'=>'error_message'
+];
+
+
+
+// return [
+
+//     'images' => [
+//         'original' => [
+//             'width' => null,
+//             'height' => null,
+//             'quality' => 100,
+//             'folder' => 'original',
+//         ],
+//         'large' => [
+//             'width' => 1200,
+//             'height' => 1200,
+//             'quality' => 80,
+//             'folder' => 'large',
+//         ],
+//         'medium' => [
+//             'width' => 800,
+//             'height' => 800,
+//             'quality' => 60,
+//             'folder' => 'medium',
+//         ],
+//         'small' => [
+//             'width' => 400,
+//             'height' => 400,
+//             'quality' => 40,
+//             'folder' => 'small',
+//         ],
+//         'thumbnail' => [
+//             'width' => 150,
+//             'height' => 150,
+//             'quality' => 30,
+//             'folder' => 'thumbnail',
+//         ],
+//     ],
+
+// ];
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/test-config', function () {
+    return response()->json(config('common.images'));
 });
